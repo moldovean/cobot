3f1c6f15547f7ddeb566a18c6a2de3ded73c0f9a
moldovean <adrian@vrabie.net>
2016-12-27 03:52:07 +0200

    some aiml updates

--- cobot-integration/src/main/java/helpers/GiphyOnline.java
+++ cobot-integration/src/main/java/helpers/GiphyOnline.java
@@ -7,6 +7,27 @@ import org.json.JSONException;
 import java.util.List;
 
 public class GiphyOnline {
+
+    public String get(String word) {
+        StringBuilder response = new StringBuilder();
+        try {
+            Translator translator = new Translator();
+
+            String gifUrl = Unirest.get("http://api.giphy.com/v1/gifs/random")
+                    .queryString("api_key", "dc6zaTOxFJmzC")
+                    .queryString("tag", translator.en(word))
+                    .asJson().getBody().getObject()
+                    .getJSONObject("data")
+                    .getString("fixed_height_small_url");
+
+            response.append("<img src=\"" + gifUrl + "\">");
+        } catch (UnirestException | JSONException e) {
+            response.append("Sau terminat toate imaginile cu " + word);
+        }
+
+        return response.toString().trim();
+    }
+
     public String get(List<String> words) {
         StringBuilder response = new StringBuilder();
         try {
--- cobot-integration/src/main/java/helpers/Translator.java
+++ cobot-integration/src/main/java/helpers/Translator.java
@@ -8,6 +8,31 @@ import org.json.JSONException;
 import java.util.List;
 
 public class Translator {
+    public String en(String word) {
+        StringBuilder response = new StringBuilder();
+        try {
+            String body = Unirest.get("https://translate.googleapis.com/translate_a/single")
+                    .queryString("client", "gtx")
+                    .queryString("sl", "ro")
+                    .queryString("tl", "en")
+                    .queryString("dt", "t")
+                    .queryString("q", String.join(" ", word))
+                    .asString()
+                    .getBody();
+
+            String translate = (new JSONArray(body))
+                    .getJSONArray(0)
+                    .getJSONArray(0)
+                    .getString(0);
+
+            response.append(translate);
+        } catch (UnirestException | JSONException e) {
+            response.append(String.join(" ", word));
+        }
+
+        return response.toString().trim();
+    }
+
     public String en(List<String> words) {
         StringBuilder response = new StringBuilder();
         try {
@@ -29,7 +54,6 @@ public class Translator {
         } catch (UnirestException | JSONException e) {
             response.append(String.join(" ", words));
         }
-
         return response.toString().trim();
     }
 }
